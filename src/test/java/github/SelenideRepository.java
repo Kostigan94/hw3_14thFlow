d32a6a5b8e0c1cf52ce160a9f76501fdf5d32b09
Kostigan94 <kostigche@gmail.com>
2022-08-09 18:08:50 +0400

    ignore

--- src/test/java/github/SelenideRepository.java
+++ src/test/java/github/SelenideRepository.java
@@ -1,16 +1,18 @@
-package Github;
+package github;
 
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideRepository {
 
+
     @BeforeAll
     static void configure() {
-        Configuration.baseUrl="https://github.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
     }
@@ -22,6 +24,13 @@ public class SelenideRepository {
         open("https://github.com/");
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
         $("ul.repo-list li").$("a").click();
-        sleep(5000);
-    }
+        $("#wiki-tab").click();
+        //Assertion
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+
+        $("#wiki-body").$(byText("Soft assertions")).click();
+        // Second Assertion
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+
+        }
 }
